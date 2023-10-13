6b742040423766240fb1be17cf8966fb2bc5ecc6
jorgsouza <jorge@vieirasouza.com.br>
2023-10-13 12:19:17 -0300

    added a connection to the postgres database within docker and the addition of reading by the application to appear in the webap

--- README.md
+++ README.md
@@ -4,7 +4,28 @@ This repository is exclusively for the study of go lang in a web application
 
 foi utilizado desta forma pois o arquivo yaml não esta na raiz
 `docker compose -f infra/compose.yaml up -d`
-`docker compose -f infra/compose.yaml down`
+`docker compose -f infra/compose.yaml down` **=>** *Eu deleto a máquina, ou seja perco tudo que foi criado no banco de dados*
 
-# access PostgresSQL
+## DataBase
+### access PostgresSQL
 `psql --host=localhost --username=postgres --port=5432`
+
+### create database
+`CREATE DATABASE store`
+
+### create a table
+`CREATE TABLE products (
+    id serial primary key,
+    name varchar,
+    description varchar,
+    price decimal,
+    quantity integer
+);`
+### insert data
+`INSERT INTO products (name,description,price,quantity) VALUES ('Glasses', 'Hyper Technology Black Glasses', 999.99, 2)`
+
+## docker stop
+`docker ps` => descobrir qual é o docker do postgres.
+`docker stop <nome do container>` **=>** *desta forma o banco não será deletado.*
+
+
--- main.go
+++ main.go
@@ -1,36 +1,67 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
 type Product struct {
+	Id                int
 	Name, Description string
 	Price             float64
 	Quantity          int
 }
 
+func databaseConnect() *sql.DB {
+	connection := "user=postgres dbname=store password=pass host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	db := databaseConnect()
+
+	selectAllProducts, err := db.Query("SELECT * FROM products")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Product{}
+	products := []Product{}
+
+	for selectAllProducts.Next() {
+		var id int
+		var name, description string
+		var price float64
+		var quantity int
+
+		err = selectAllProducts.Scan(&id, &name, &description, &price, &quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Name = name
+		p.Description = description
+		p.Price = price
+		p.Quantity = quantity
+
+		products = append(products, p)
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
+
+	defer db.Close()
+}
 
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-
-	products := []Product{
-		{
-			Name:        "T-Shirt",
-			Description: "A coll t-shirt",
-			Price:       29.99,
-			Quantity:    9,
-		},
-		{"Hood", "A colorful hood", 199.99, 2},
-		{"Fone", "Hyper tech Fone", 1999.89, 3},
-		{"New secret product", "a new secret for a great person", 91999.89, 6},
-	}
-
-	temp.ExecuteTemplate(w, "Index", products)
 }
