055061574cccaa2e03588512a25d36d078e7364e
jorgsouza <jorge@vieirasouza.com.br>
2023-10-13 16:55:31 -0300

    New page in route

--- controllers/products.go
+++ controllers/products.go
@@ -13,3 +13,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	allProducts := models.SearchAllProducts()
 	temp.ExecuteTemplate(w, "Index", allProducts)
 }
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
