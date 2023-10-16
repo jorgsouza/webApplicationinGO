4a6d8dab7a0c3aacd7345d353ae80953c647349c
jorgsouza <jorge@vieirasouza.com.br>
2023-10-15 21:01:14 -0300

    create .env to store credencials

--- db/db.go
+++ db/db.go
@@ -2,12 +2,26 @@ package db
 
 import (
 	"database/sql"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file")
+	}
+}
+
 func DatabaseConnect() *sql.DB {
-	connection := "user=postgres dbname=postgres password=pass host=localhost sslmode=disable"
+	dbUser := os.Getenv("BD_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbMode := os.Getenv("DB_MODE")
+
+	connection := "user=" + dbUser + "dbname=" + dbName + "password=" + dbPassword + "host=" + dbHost + "sslmode=" + dbMode
 	db, err := sql.Open("postgres", connection)
 	if err != nil {
 		panic(err.Error())
